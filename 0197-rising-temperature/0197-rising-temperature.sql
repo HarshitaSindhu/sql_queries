6cfa2dfec4f90952d3cef82de21e1ac4aaaf986c
Harshita Sindhu <harshitasindhu5120@gmail.com>
2025-10-11 21:24:35 +0530

    Time: 438 ms (75.43%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -4,3 +4,13 @@ FROM Weather w1
 Join Weather w2
 ON datediff(w1.recordDate , w2.recordDate) = 1
 and w1.temperature > w2.temperature
+
+-- “link the row from w1 to the row from w2 if the date difference is exactly 1 day.”
+
+-- Hum same table ko do baar use kar rahe hain — ek “aaj” (w1) aur ek “kal” (w2) ke liye.
+-- DATEDIFF = 1 matlab: w1 wali date, w2 se ek din baad hai.
+
+-- Fir hum dekhte hain: agar aaj ka temperature kal se zyada hai,
+-- to hum w1.id ko output me le lete hain.
+
+-- Isliye output me 2 aur 4 aata hai (kyunki un dinon temperature bada tha).
